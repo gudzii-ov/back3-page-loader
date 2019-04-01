f836304eb7ae8e568062e53ad10b5b1ebba874b6
Oleg Gduzii <oleg198679@gmail.com>
2019-04-01 15:04:14 +0300

    Refactor tests

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -16,23 +16,47 @@ beforeAll(async () => {
   data.host = 'http://localhost';
   data.simplePageUrl = '/simple-page';
   data.simplePageHtml = await fs.readFile(path.join(__dirname, '__fixtures__/simple-page.html'), 'utf8');
-
-  nock(data.host)
-    .log(console.log)
-    .get(data.simplePageUrl)
-    .reply(200, data.simplePageHtml);
 });
 
-test('test#1: download html without assets', async () => {
-  const {
-    ostmpdir, host, simplePageUrl, simplePageHtml,
-  } = data;
+describe('download simple page without assets', () => {
+  beforeEach(() => {
+    nock(data.host)
+      .log(console.log)
+      .get(data.simplePageUrl)
+      .reply(200, data.simplePageHtml);
+  });
 
-  const tmpDir = await fs.mkdtemp(path.join(ostmpdir, 'page-loader-'));
-  const fileName = 'localhost-simple-page.html';
-  const filePath = path.resolve(tmpDir, fileName);
+  test('test#1: download html without assets to current directory', async () => {
+    const {
+      ostmpdir, host, simplePageUrl, simplePageHtml,
+    } = data;
 
-  await app(`${host}${simplePageUrl}`, tmpDir);
-  const result = fs.readFile(filePath, 'utf8');
-  return expect(result).resolves.toBe(simplePageHtml);
+    const tmpDir = await fs.mkdtemp(path.join(ostmpdir, 'page-loader-'));
+    const fileName = 'localhost-simple-page.html';
+    const filePath = path.resolve(tmpDir, fileName);
+
+    try {
+      process.chdir(tmpDir);
+    } catch (err) {
+      console.log(`Unable to change directory: ${err}`);
+    }
+
+    await app(`${host}${simplePageUrl}`, process.cwd());
+    const result = fs.readFile(filePath, 'utf8');
+    return expect(result).resolves.toBe(simplePageHtml);
+  });
+
+  test('test#2: download html without assets to custom directory', async () => {
+    const {
+      ostmpdir, host, simplePageUrl, simplePageHtml,
+    } = data;
+
+    const tmpDir = await fs.mkdtemp(path.join(ostmpdir, 'page-loader-'));
+    const fileName = 'localhost-simple-page.html';
+    const filePath = path.resolve(tmpDir, fileName);
+
+    await app(`${host}${simplePageUrl}`, tmpDir);
+    const result = fs.readFile(filePath, 'utf8');
+    return expect(result).resolves.toBe(simplePageHtml);
+  });
 });
